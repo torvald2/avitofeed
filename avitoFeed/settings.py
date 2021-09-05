7524f8afd7d957b4d74a83dccf03ebd0161044d4
denisskripnik <valis.source@gmail.com>
2021-09-05 11:25:50 +0300

    fix settings

--- avitoFeed/settings.py
+++ avitoFeed/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +131,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "client/dist"),
 ]
+django_heroku.settings(locals())
